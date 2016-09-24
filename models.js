773155f3b5f5823df51b7ff67b6c611e97dd7337
김현준 <nfox0506@gmail.com>
2016-09-24 21:20:14 +0900

    외부 서버로 변경

--- models.js
+++ models.js
@@ -1,5 +1,13 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('node_api_codelab', 'root', 'nfox0506')
+const sequelize = new Sequelize('node_api_codlab', 'root', 'guswns12',{
+  host:'183.109.83.141',
+  port:5070,
+  pool:{
+    max:5,
+    min:0,
+    idle:10000//접속 후 얼마나 시간을 줄지
+  }
+});
 
 const User = sequelize.define('user', {
   name: Sequelize.STRING
