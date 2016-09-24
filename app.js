236f2314d4188c6b510071aeca6e5676885ace6f
김현준 <nfox0506@gmail.com>
2016-09-24 21:58:38 +0900

    스웨거-ui라이브러리 사용 swagger작동확인

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const bodyParser=require('body-parser');
@@ -8,7 +9,10 @@ const bodyParser=require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/users', require('./api/user'));
-
+app.use('/swg-doc', express.static(path.join(__dirname, './node_modules/swagger-ui/dist')));
+app.use('/v1/swagger.json',(req,res)=>{
+  res.json(require('./swagger.json'));
+});
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
